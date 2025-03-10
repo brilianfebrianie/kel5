b5f33b0fc617bb086c60d43cc2644d188c301ed8
Andika Naufan <andikanaufannd@gmail.com>
2025-03-10 11:35:06 +0700

    added local time

--- python/app/models/client.py
+++ python/app/models/client.py
@@ -1,10 +1,13 @@
 from app import db
 from uuid import uuid4
 from datetime import datetime
+import pytz
+
+LOCAL_TIMEZONE = pytz.timezone("Asia/Jakarta") 
 
 class ClientDummy(db.Model):
     __tablename__ = 'client_dummy' 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid4()))
     nama = db.Column(db.String(255), nullable=False)
     alamat = db.Column(db.String(255), nullable=False)
-    created_time = db.Column(db.DateTime, default=datetime.utcnow)
+    created_time = db.Column(db.DateTime, default=lambda: datetime.now(LOCAL_TIMEZONE))
--- python/app/models/log.py
+++ python/app/models/log.py
@@ -2,10 +2,13 @@ from app import db
 from uuid import uuid4
 from datetime import datetime
 from sqlalchemy import text
+import pytz
+
+LOCAL_TIMEZONE = pytz.timezone("Asia/Jakarta") 
 
 class ApiLog(db.Model):
     __tablename__ = 'api_logs'
     api_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid4()))
     request_payloads = db.Column(db.Text, nullable=False)
     response_payloads = db.Column(db.Text, nullable=False)
-    created_time = db.Column(db.DateTime, default=datetime.utcnow)
+    created_time = db.Column(db.DateTime, default=lambda: datetime.now(LOCAL_TIMEZONE))
